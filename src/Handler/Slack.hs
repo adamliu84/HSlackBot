fd17a702287cf20be02daf8765a7d8971ff848cd
adamliu84 <adam.liu84@gmail.com>
2020-10-24 18:58:31 +0800

    Event registeration verification

--- src/Handler/Slack.hs
+++ src/Handler/Slack.hs
@@ -12,6 +12,9 @@ import qualified Data.Text.Encoding (encodeUtf8)
 import qualified Data.ByteString.Lazy.Char8 as LB (ByteString, pack)
 import Data.Aeson
 import qualified Yesod.Core.Json as YJ
+import Data.Maybe (fromJust)
+
+data EventType = UrlVerification {challenge::Text} | EventTypeError deriving (Show)
 
 base_url :: String
 base_url = "https://slack.com/api/"
@@ -21,13 +24,30 @@ sample_body_json = LB.pack "{\"channel\":\"#testing\",\"text\":\"A API message f
 
 postSlackR :: Handler YJ.Value
 postSlackR = do
-        response <- liftIO $ sendMessage
-        returnJson response
+        curbody <- requireCheckJsonBody :: Handler Value
+        let eventType = getEventType curbody
+        case eventType of
+            UrlVerification c -> do
+                returnJson $ "{'challenge':'"++ c ++"'}"
+            _                 -> do
+                response <- liftIO $ sendMessage
+                returnJson response
+
 {-
 SLACK
 |-}
 getSlackAuthToken :: ByteString
 getSlackAuthToken = "Bearer " ++ (Data.Text.Encoding.encodeUtf8 $ slackAuthToken compileTimeAppSettings)
+
+getEventType :: Value -> EventType
+getEventType (Object l) = fromJust $ isUrlVerification l <|> (Just EventTypeError)
+getEventType _ = EventTypeError
+
+isUrlVerification :: HashMap Text Value -> Maybe EventType
+isUrlVerification l = do
+    _ <- lookup "type" l
+    (String c) <- lookup "challenge" l
+    return $ UrlVerification c
 
 sendMessage :: IO Object
 sendMessage = do
